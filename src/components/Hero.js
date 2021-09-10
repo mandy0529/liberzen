b895d2715507b28bd0330ee4457bac926147a9a8
Minji <321963741@naver.com>
2021-09-11 07:43:59 +0900

    again video home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,43 +1,44 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {useGlobalContext} from './Context';
-import {VscUnmute, VscMute} from 'react-icons/vsc';
+import {VscUnmute, VscMute, VscDebugPause} from 'react-icons/vsc';
+import {FiPlay} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 import Mobile from './Mobile';
 
 const Hero = () => {
   const {closeSidebar} = useGlobalContext();
   const [muted, setMuted] = useState(true);
+  const [play, setPlay] = useState(true);
   const [mobile, setMobile] = useState(false);
   const soundRef = useRef();
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const inner = window.innerWidth;
-      if (inner >= 768) {
-        setMobile(false);
-      } else {
-        setMobile(true);
-      }
-    };
-    window.addEventListener('load', handleScroll);
-    return () => window.removeEventListener('load', handleScroll);
-    // eslint-disable-next-line
-  }, []);
-  useEffect(() => {
-    const handleScroll = () => {
-      const inner = window.innerWidth;
-      if (inner >= 768) {
-        setMobile(false);
-      } else {
-        setMobile(true);
-      }
-    };
-    window.addEventListener('resize', handleScroll);
-    return () => window.removeEventListener('resize', handleScroll);
-    // eslint-disable-next-line
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const inner = window.innerWidth;
+  //     if (inner >= 768) {
+  //       setMobile(false);
+  //     } else {
+  //       setMobile(true);
+  //     }
+  //   };
+  //   window.addEventListener('load', handleScroll);
+  //   return () => window.removeEventListener('load', handleScroll);
+  //   // eslint-disable-next-line
+  // }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const inner = window.innerWidth;
+  //     if (inner >= 768) {
+  //       setMobile(false);
+  //     } else {
+  //       setMobile(true);
+  //     }
+  //   };
+  //   window.addEventListener('resize', handleScroll);
+  //   return () => window.removeEventListener('resize', handleScroll);
+  //   // eslint-disable-next-line
+  // }, []);
 
-  console.log(mobile, 'what mode now');
   const handleClick = () => {
     const isMuted = soundRef.current.muted;
     if (isMuted) {
@@ -46,6 +47,16 @@ const Hero = () => {
     } else {
       soundRef.current.muted = true;
       setMuted(true);
+    }
+  };
+
+  const handlePlay = () => {
+    if (soundRef.current.paused) {
+      soundRef.current.play();
+      setPlay(true);
+    } else {
+      soundRef.current.pause();
+      setPlay(false);
     }
   };
 
@@ -76,10 +87,17 @@ const Hero = () => {
                 <VscUnmute className="mute-icon" />
               )}
             </div>
+            <div onClick={handlePlay} className="play">
+              {play ? (
+                <FiPlay className="play-icon" />
+              ) : (
+                <VscDebugPause className="play-icon" />
+              )}
+            </div>
           </>
         )}
         {/* Mobile */}
-        {mobile && <Mobile />}
+        {/* {mobile && <Mobile />} */}
         <Link to="/shop" className="go-shop">
           Go shop
         </Link>
